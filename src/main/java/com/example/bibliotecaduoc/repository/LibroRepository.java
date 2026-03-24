e3f1cada6d0bc5ed55c053a20ecdc2b2e849e861
Cristian <c.fernandezu@duocuc.cl>
2026-03-24 11:30:19 -0300

    correcciones

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -26,64 +26,40 @@ public class LibroRepository {
         return null;
     }
 
-    public Libro BuscarPorIsbn(String isbn){
-        for (Libro libro : listaLibros){
-            if (libro.getIsbn() .equals(isbn)){
-                 return libro;
+    public Libro buscarPorIsbn(String isbn) {
+        for (Libro libro : listaLibros) {
+            if (libro.getIsbn().equals(isbn)) {
+                return libro;
             }
         }
         return null;
     }
 
-    public Libro guardar (Libro lib){
-        listaLibros.add(lib){
-            return.lib;
+    public Libro guardar (Libro book){
+        listaLibros.add(book){
+            return book;
         }
     }
 
-    public Libro actualizar(Libro lib){
-        int id = 0;
-        int idPosicion = 0;
+    public Libro actualizar(Libro book){
+        Libro libroBuscado=buscarPorId(book.getId());
+        if (libroBuscado!=null) {
+        libroBuscado.setIsbn(book.getIsbn());
+        libroBuscado.setAutor(book.getAutor());
+        libroBuscado.setEditorial(book.getEditorial());
+        libroBuscado.setFechaPublicacion(book.getFechaPublicacion());
+        libroBuscado.setTitulo(book.getTitulo());
 
-        for (int i = 0; i < listaLibros.size(); i++){
-            if (listaLibros.get(i).getId()== lib.getId() ){
-                id = lib.getId();
-                idPosicion = i ;
-            }
+        return libroBuscado;
         }
-
-        Libro libro1 = new Libro();
-        libro1.setId(id);
-        libro1.setTitulo(lib.getTitulo());
-        libro1.setAutor(lib.getAutor());
-        libro1.setFechaPublicacion(lib.getFechaPublicacion());
-        libro1.setEditorial(lib.getEditorial());
-        libro1.setIsbn(lib.getIsbn());
-
-        listaLibros.set(idPosicion, libro1);
-        return libro1;
-
+        return null;
     }
+
+    
 
     public void eliminar(int id) {
-        Libro libro = buscarPorId(id);
-        if (libro != null) {
-            listaLibros.remove(libro);
-        }
-
-        int idPosicion = 0;
-        for (int i = 0; i < listaLibros.size(); i++) {
-            if (listaLibros.get(i).getId() == id) {
-                idPosicion = i;
-                break;
-            }
-        }
-        if (idPosicion > 0) {
-            listaLibros.remove(idPosicion);
-
-        }
-        listaLibros.removeIf(x -> x.getId() == id);
-
+        listaLibros.removeIf(book->book.getId()==id);
+        
     }
 
 
